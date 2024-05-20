d6f238e5baf20cb8d5ebb22a1362ae75da836e8b
Yoel Villa <villayoel@gmail.com>
2024-05-20 02:45:43 +0200

    - Improve the styles

---
+++ src/app/components/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  year:Number = 0
+
+  ngOnInit():void{
+    this.year = new Date().getFullYear()
+  }
+
+
+}
